e97e06fee69e16bf0c67889cc819ac166c44f8bb
kelly <kiroegbu@gmail.com>
2026-01-29 15:11:48 +0100

    Add

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -24,12 +24,11 @@ export default function Index() {
             </View>
 
             <View style={styles.textGroup}>
-              <Text style={styles.textLarge}>Find your favorite place here</Text>
-              <Text style={styles.textSmall}>The best prices for over 2 </Text>
-              <Text style={styles.textSmall}>
-                million properties worldwide
-              </Text>
-            </View>
+                <Text style={styles.textLarge}>Find your favorite place here</Text>
+                <Text style={styles.textSmall}>The best prices for over 2 </Text>
+                <Text style={styles.textSmall}>million properties worldwide</Text>
+                </View>
+
 
             <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
               <View style={styles.buttonGroup}>
